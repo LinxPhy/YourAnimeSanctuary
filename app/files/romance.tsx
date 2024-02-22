427e5fd1a2723c4212f1890cff526f95fd8c479b
LinxPhy <lin.aboagye@gmail.com>
2024-02-22 22:14:50 +0000

    6 Romance

---
+++ app/files/romance.tsx
@@ -0,0 +1,90 @@
+
+const Romance = [
+
+    {
+        id: 1,
+        title: "Tonikaku Kawaii",
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2Ftonikaku.jpg?alt=media&token=26efd659-ea04-47c3-ad75-38986bff3226',
+        genre: ['Comedy', 'Romance'],
+        episodes: 12,
+        seasons: 2,
+        type: 'TV',
+        cruncyroll: 'https://www.crunchyroll.com/series/GRWMGGQ86/tonikawa-over-the-moon-for-you',
+        youtube: 'https://www.youtube.com/watch?v=de9OckSLP1o',
+        description: "Tonikaku Kawaii is an absolute gem of an anime that stole my heart from the very first episode! I was swept away by the sweet and pure love story between Nasa and Tsukasa. The animation was stunning, and every episode left me with a warm and fuzzy feeling inside. The humor had me laughing out loud, the romance had me swooning, and the overall feel-good vibes had me hooked until the very end. Their unwavering love and dedication to each other is truly inspiring and heartwarming. I found myself rooting for their relationship every step of the way, and the ending left me feeling emotional and satisfied. Tonikaku Kawaii is a must-watch for anyone who loves a good love story filled with sweetness, humor, and tons of heart!",
+        url: 'https://myanimelist.net/anime/41389/Tonikaku_Kawaii',
+    },
+
+    {
+        id: 2,
+        title: 'Toradora',
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2Ftoradora.webp?alt=media&token=596e1fc8-6a46-48f3-a017-f8c654a6c54c',
+        genre: ['Drama', 'Romance'],
+        episodes: 25,
+        seasons: 1,
+        type: 'TV',
+        cruncyroll: 'https://www.crunchyroll.com/series/G6P8PXJW6/toradora',
+        youtube: 'https://www.youtube.com/watch?v=ya570uUgQNc',
+        description: "Toradora is an absolutely heartwarming and charming anime that completely stole my heart. From the lovable and complex characters to the bittersweet moments of love and friendship, this show is a rollercoaster of emotions that will have you laughing, crying, and cheering for the characters every step of the way. The romantic tension between the fiery Taiga and the seemingly cold-hearted Ryuuji is beautifully portrayed, keeping you on the edge of your seat until the very end. The character development is simply phenomenal, making you feel deeply connected to each of them. Toradora is a true masterpiece that delves into the complexities of relationships and growing up, leaving a lasting impact on anyone who watches it. Trust me, you don't want to miss out on this unforgettable journey of love and self-discovery!",
+        url: 'https://myanimelist.net/anime/4224/Toradora',
+    },
+
+    {
+        id: 3,
+        title: 'My Teen Romantic Comedy SNAFU',
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2Fyahari.jpg?alt=media&token=40441570-6973-44a8-b260-dceca34a965c',
+        genre: ['Comedy', 'Romance'],
+        episodes: 13,
+        seasons: 3,
+        type: 'TV',
+        cruncyroll: 'https://www.crunchyroll.com/series/GYNQJ8GXY/my-teen-romantic-comedy-snafu',
+        youtube: 'https://www.youtube.com/watch?v=u-bpwWPNEpE',
+        description: 'An emotional rollercoaster that delves deep into the complexities of human relationships with raw honesty and relatability. The protagonist, Hachiman, is a breath of fresh air with his cynical yet insightful outlook on life, making him a captivating character to follow. The dynamics between the well-written cast create a rich tapestry of emotions, from heart-wrenching moments of vulnerability to heartwarming displays of friendship. The anime effortlessly blends comedy, drama, and romance, keeping viewers on the edge of their seats with its thought-provoking storyline. Ultimately, My Teen Romantic Comedy SNAFU is a masterpiece that will not only tug at your heartstrings but also leave a lasting impact on your soul.',
+        url: 'https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru',
+    },
+
+    {
+        id: 4,
+        title: '3D Kanojo: Real Girl',
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2F3d_kanojo.jpg?alt=media&token=cb1ca84f-15e8-4197-93fd-2a67634acf56',
+        genre: ['Romance'],
+        episodes: 12,
+        seasons: 2,
+        type: 'TV',
+        cruncyroll: 'https://www.hidive.com/tv/real-girl',
+        youtube: 'https://www.youtube.com/watch?v=4b1Lpm_px04',
+        description: "rom the sweet and wholesome romance between the main characters to the beautiful character development and emotional moments, this anime truly captures the essence of young love in all its awkwardness and beauty. The animation is stunning, bringing the characters to life in a way that makes you feel like you're right there with them on their journey. Watching their love story unfold is a rollercoaster of emotions, from heartwarming moments to heart-wrenching ones that will have you reaching for the tissues. 3D Kanojo is a must-watch for anyone who loves a good romance filled with passion, love, and lots of lovely-goowey feelings",
+        url: 'https://myanimelist.net/anime/36793/3D_Kanojo__Real_Girl',
+    },
+
+    {
+        id: 5,
+        title: 'Golden Time',
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2Fgolden_time.jpg?alt=media&token=a1a1c75a-9256-4369-ba70-63a9face6bed',
+        genre: ['Comedy', 'Romance'],
+        episodes: 24,
+        seasons: 1,
+        type: 'TV',
+        cruncyroll: 'https://www.hidive.com/tv/golden-time',
+        youtube: 'https://www.youtube.com/watch?v=ER8wXRhZW1k',
+        description: "The story follows Tada Banri, a college student who loses his memories but finds love and friendship in the most unexpected places. The characters are so well-developed and relatable that you can't help but become invested in their journeys. The romantic moments are so tender and touching, you can't help but believe in the power of love. The show beautifully captures the complexities of relationships and personal growth, making it a truly unforgettable and moving experience.",
+        url: 'https://myanimelist.net/anime/17895/Golden_Time',
+    },
+
+    {
+        id: 6,
+        title: 'Plastic Memories',
+        image: 'https://firebasestorage.googleapis.com/v0/b/anime-sanctuary-bb873.appspot.com/o/images%2Fplastic_memories.jpg?alt=media&token=68690798-71f6-4bee-87af-6ee567dab5b1',
+        genre: ['Drama', 'Romance', 'Sci-Fi'],
+        episodes: 13,
+        seasons: 1,
+        type: 'TV',
+        cruncyroll: 'https://www.crunchyroll.com/series/GRJQVX93Y/plastic-memories',
+        youtube: 'https://www.youtube.com/watch?v=vk6mg74R5Ek',
+        description: 'This beautiful series tells the heartwarming story of Tsukasa and Isla, two characters whose deepening connection despite the heart-wrenching circumstances will tug at your heartstrings like never before. The poignant exploration of love, loss, and the fleeting nature of time will leave you reflecting on the precious moments in life and cherishing every single one. With its stunning animation, touching soundtrack, and unforgettable characters, Plastic Memories s a must-watch that will make you laugh, cry, and ultimately feel a warmth in your soul that lingers long after the final episode.',
+        url: 'https://myanimelist.net/anime/27775/Plastic_Memories',
+    },
+
+]
+
+export default Romance;
---
+++ app/romance/page.tsx
@@ -0,0 +1,29 @@
+import RomancePage from '@/app/files/romance'
+import Anime from '../components/anime'
+
+export default function Romance() {
+
+    return (
+        <main className="Romance">
+
+            <div className='title-section'>
+                <h1>10 Romance Anime you need to watch</h1>
+            </div>
+
+            <div className='page-image-section'>
+
+            </div>
+
+            <div className='page-description'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto mollitia obcaecati laudantium sunt inventore ab placeat totam perferendis sit animi debitis, reiciendis voluptas esse nesciunt architecto libero, fuga voluptatum! Blanditiis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis distinctio reiciendis omnis adipisci impedit illo nam sequi. Consectetur consequatur nam accusamus totam quod numquam eius, molestiae quos voluptas sequi similique.</p>
+            </div>
+
+         
+            <Anime data={RomancePage} />
+            
+        </main>
+    )
+
+}
+
